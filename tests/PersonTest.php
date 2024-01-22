50f6dc9e008f43a235279320ff90e8e94f74d294
Iridilia <97386524+Iridilia@users.noreply.github.com>
2024-01-22 17:47:12 +0100

    Modification du fichier PersonTest.php : ajout des test des messages Exception

--- tests/PersonTest.php
+++ tests/PersonTest.php
@@ -42,14 +42,20 @@ class PersonTest extends TestCase
     }
 
     public function testTransfertFund():void {
+        $this->expectException(\Exception::class);
+        $this->expectExceptionMessage('Can\'t give money with different currencies');
         $person1=new Person('Lola', 'EUR');
         $person2=new Person('Emilie', 'EUR');
+        $person3=new Person('John', 'USD');
+
         $person1->wallet->setBalance(100);
         $person1->transfertFund(40, $person2);
+        $person1->transfertFund(20, $person3);
         // Lola avait 100€, elle en a transféré 40 à Lola. Elle devrait avoir 60€ sur son compte.
         $this->assertEquals(60, $person1->wallet->getBalance());
         // Emilie avait 0€, elle en a reçu 40 de Lola. Elle devrait avoir 40€ sur son compte.
         $this->assertEquals(40, $person2->wallet->getBalance());
+        // John et Lola n'ont pas la même currency. Ils ne peuvent pas faire de transfert d'argent entre eux.
     }
 
     public function testDivideWallet():void {
@@ -70,10 +76,14 @@ class PersonTest extends TestCase
     }
 
     public function testBuyProduct():void {
+        $this->expectException(\Exception::class);
+        $this->expectExceptionMessage('Can\'t buy product with this wallet currency');
         $person1=new Person('Lola', 'EUR');
         $person1->wallet->setBalance(200);
-        $product=new Product('Bague', ['USD'=>163.00, 'EUR'=>150.00], 'other');
-        $person1->buyProduct($product);
+        $product1=new Product('Bague', ['USD'=>163.00, 'EUR'=>150.00], 'other');
+        $product2=new Product('Pomme', ['USD'=>1], 'food');
+        $person1->buyProduct($product1);
+        $person1->buyProduct($product2);
         $this->assertEquals(50, $person1->wallet->getBalance());
 
     }
